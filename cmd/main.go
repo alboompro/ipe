11733473ceb8e9142cc6e2c38b4cc619a4c829d5
Elison de Campos <elison.campos@gmail.com>
2025-11-28 14:13:42 -0300

    :art: another ascii

--- cmd/main.go
+++ cmd/main.go
@@ -24,38 +24,25 @@ func main() {
 
 // Print a beautiful banner
 func printBanner() {
-	// Gradient colors from Alboom brand - hot pink to purple to cyan
-	// Using true color (24-bit) ANSI escape codes for smooth gradients
+	// Gradient colors from Alboom brand - pink to purple to teal to cyan
+	// Using true color (24-bit) ANSI escape codes
 	r := "\033[0m"
 
-	// Color stops for gradient: Pink -> Purple -> Teal -> Cyan
-	c := []string{
-		"\033[38;2;238;53;142m", // #EE358E pink
-		"\033[38;2;200;50;140m", // transition
-		"\033[38;2;162;47;138m", // transition
-		"\033[38;2;124;52;136m", // transition
-		"\033[38;2;95;52;148m",  // #5F3494 purple
-		"\033[38;2;85;140;160m", // transition
-		"\033[38;2;85;175;165m", // transition
-		"\033[38;2;85;194;171m", // #55BFAB teal
-		"\033[38;2;94;194;238m", // #5EC2EE cyan
-	}
+	// Brand colors
+	pink := "\033[38;2;238;53;142m"  // #EE358E
+	purple := "\033[38;2;95;52;148m" // #5F3494
+	teal := "\033[38;2;85;191;171m"  // #55BFAB
+	cyan := "\033[38;2;94;194;238m"  // #5EC2EE
 
 	fmt.Println()
-	fmt.Println()
 
-	// ALBOOM IPÊ - Brutal gradient ASCII art
-	fmt.Printf("  %s█████%s  %s██%s      %s████%s    %s█████%s   %s█████%s  %s███%s   %s███%s    %s████%s  %s████%s   %s█████%s\n",
-		c[0], r, c[1], r, c[2], r, c[3], r, c[4], r, c[5], r, c[6], r, c[7], r, c[7], r, c[8], r)
-	fmt.Printf(" %s██   ██%s %s██%s      %s██  ██%s  %s██   ██%s %s██   ██%s %s████%s %s████%s   %s██%s    %s██  ██%s %s██%s\n",
-		c[0], r, c[1], r, c[2], r, c[3], r, c[4], r, c[5], r, c[6], r, c[7], r, c[7], r, c[8], r)
-	fmt.Printf(" %s███████%s %s██%s      %s██████%s  %s██   ██%s %s██   ██%s %s██%s %s██%s %s██%s   %s██%s    %s██████%s %s████%s\n",
-		c[0], r, c[1], r, c[2], r, c[3], r, c[4], r, c[5], r, c[5], r, c[6], r, c[7], r, c[7], r, c[8], r)
-	fmt.Printf(" %s██   ██%s %s██%s      %s██   ██%s %s██   ██%s %s██   ██%s %s██%s     %s██%s   %s██%s    %s██%s      %s██%s\n",
-		c[0], r, c[1], r, c[2], r, c[3], r, c[4], r, c[5], r, c[6], r, c[7], r, c[7], r, c[8], r)
-	fmt.Printf(" %s██   ██%s %s██████%s %s████████%s %s█████%s   %s█████%s  %s██%s     %s██%s   %s████%s  %s██%s     %s█████%s\n",
-		c[0], r, c[1], r, c[2], r, c[3], r, c[4], r, c[5], r, c[6], r, c[7], r, c[7], r, c[8], r)
+	// Alboom Ipê - using box drawing for clean look
+	fmt.Printf(" %s╔═╗%s %s╦%s   %s╔╗%s  %s╔═╗%s %s╔═╗%s %s╔╦╗%s   %s╦%s %s╔═╗%s %s╔═╗%s\n",
+		pink, r, pink, r, purple, r, purple, r, teal, r, teal, r, cyan, r, cyan, r, cyan, r)
+	fmt.Printf(" %s╠═╣%s %s║%s   %s╠╩╗%s %s║ ║%s %s║ ║%s %s║║║%s   %s║%s %s╠═╝%s %s╠╣%s\n",
+		pink, r, pink, r, purple, r, purple, r, teal, r, teal, r, cyan, r, cyan, r, cyan, r)
+	fmt.Printf(" %s╩ ╩%s %s╩═╝%s %s╚═╝%s %s╚═╝%s %s╚═╝%s %s╩ ╩%s   %s╩%s %s╩%s   %s╚═╝%s\n",
+		pink, r, pink, r, purple, r, purple, r, teal, r, teal, r, cyan, r, cyan, r, cyan, r)
 
-	fmt.Println()
 	fmt.Println()
 }
